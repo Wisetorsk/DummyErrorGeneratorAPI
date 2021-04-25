fcdae145f88428c5e398d2e73d056f2d2bb34a71
Marius Kristiansen <thewisetorsk@gmail.com>
2021-04-25 13:49:35 +0200

    Moved Error data grid to external component library

---
+++ ErrorGenerator.Components/Component1.razor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorGenerator.Components
+{
+    public partial class Component1
+    {
+        public int number = 100;
+    }
+}
